8316e73835f7bcf4d1e544604afab9ea2e1e88d9
hyeon924 <hyeonn4713@gmail.com>
2025-11-25 15:30:45 +0900

    log front

--- front/src/app/users/login/page.tsx
+++ front/src/app/users/login/page.tsx
@@ -16,6 +16,8 @@ export default function LoginPage() {
 
   const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
+  console.log('API_BASE_URL >>>', API_BASE_URL);
+
   if (!API_BASE_URL) {
     console.error('NEXT_PUBLIC_API_BASE_URL이 설정되지 않았습니다.');
   }
